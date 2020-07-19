e22ba6cc6315e9a9117f930ac58efbbbaf3b1671
Elliott <enix002@aucklanduni.ac.nz>
2020-07-19 15:02:56 +1200

    orders only show that user's orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -67,8 +67,9 @@ export const fetchOrdersFail = (error) => {
 export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo"' + userId + '"';
-        axios.get('/orders.json?auth=' + queryParams)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        console.log('/orders.json' + queryParams);
+        axios.get('/orders.json' + queryParams)
         .then(res => {
             const fetchedOrders = [];
             for (let key in res.data) {
